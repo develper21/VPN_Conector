84dd62cc1134811c64b831022b8d532b29db004c
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-07 15:22:30 +0530

    Add tests module initialization

---
+++ vpn_network/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+Test package for the VPN Security Project.
+
+This package contains all the test modules for the VPN Security Project.
+"""
+
+# This file makes the tests directory a Python package
+# It can be empty, but it's needed for Python to recognize the directory as a package
+
+# You can also define any test fixtures or common test utilities here
