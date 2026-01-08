59b59254cbc368030de172a717f98b2a3e59d3d4
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-08 23:55:27 +0530

    Add ui __init__.py

--- vpn_network/src/ui/__init__.py
+++ vpn_network/src/ui/__init__.py
@@ -1,1 +1,24 @@
-"""Desktop management UI package for the VPN project."""
+#!/usr/bin/env python3
+"""
+Enhanced UI Package for VPN
+Modern interface components with server location map, speed graphs, and one-click selection.
+"""
+
+from .enhanced_ui import EnhancedVPNGUI
+from .server_map_widget import AdvancedServerMapWidget, MapServer
+from .speed_graph_widget import AdvancedSpeedGraphWidget, SpeedDataPoint, SpeedStatistics
+from .server_selector import SmartServerSelector, ServerMetrics, RecommendationScore
+
+__all__ = [
+    'EnhancedVPNGUI',
+    'AdvancedServerMapWidget',
+    'MapServer',
+    'AdvancedSpeedGraphWidget',
+    'SpeedDataPoint',
+    'SpeedStatistics',
+    'SmartServerSelector',
+    'ServerMetrics',
+    'RecommendationScore'
+]
+
+VERSION = "1.0.0"
